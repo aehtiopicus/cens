d69d44be889c044e941493f1f73503371f42724b
jvega <jvega@belatrixsf.com>
2015-03-05 19:15:46 -0300

    changes

--- cens_web/src/main/java/com/aehtiopicus/cens/configuration/SchedulerConfiguration.java
+++ cens_web/src/main/java/com/aehtiopicus/cens/configuration/SchedulerConfiguration.java
@@ -1,8 +1,8 @@
 package com.aehtiopicus.cens.configuration;
 
-import org.quartz.CronTrigger;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
+import org.quartz.JobDataMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,29 +10,61 @@ import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
+import com.aehtiopicus.cens.service.cens.MiembroCensService;
+
 @Configuration
 @EnableScheduling
 public class SchedulerConfiguration {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+		
+	
 	@Bean
-	public CronTriggerFactoryBean getTrigger(){
+	public CronTriggerFactoryBean getMorningNotification(){
 		CronTriggerFactoryBean ct = new CronTriggerFactoryBean();
-		ct.setCronExpression("*/5 * *  * * ?");
-		ct.setJobDetail(getJob().getObject());
+		ct.setCronExpression("10 * *  * * ?");
+		ct.setDescription("manana");
+		ct.setJobDetail(getEmailNotificationJob().getObject());
 		return ct;
 		
 	}
+	
 	@Bean
-	public JobDetailFactoryBean getJob(){
+	public CronTriggerFactoryBean getNoonModification(){
+		CronTriggerFactoryBean ct = new CronTriggerFactoryBean();
+		ct.setCronExpression("25 * *  * * ?");
+		ct.setDescription("medio dia");
+		ct.setJobDetail(getEmailNotificationJob().getObject());
+		return ct;
+		
+	}
+	
+	@Bean
+	public CronTriggerFactoryBean getNightModification(){
+		CronTriggerFactoryBean ct = new CronTriggerFactoryBean();
+		ct.setCronExpression("35 * *  * * ?");
+		ct.setDescription("noche");
+		ct.setJobDetail(getEmailNotificationJob().getObject());
+		return ct;
+		
+	}
+	
+	@Bean
+	public JobDetailFactoryBean getEmailNotificationJob(){
 		JobDetailFactoryBean jdfb = new JobDetailFactoryBean();
 		jdfb.setJobClass(TestScheduler.class);
+		Object obj = applicationContext.getBean(MiembroCensService.class);
+		JobDataMap jdm = new JobDataMap();
+		jdm.put("miembroCensService", obj);
+		jdfb.setJobDataMap(jdm);
 		return jdfb;
 	}
 	
 	@Bean
 	public SchedulerFactoryBean getScheduler(){
 		SchedulerFactoryBean sfb =new SchedulerFactoryBean();
-		sfb.setTriggers(getTrigger().getObject());
+		sfb.setTriggers(getNightModification().getObject(),getMorningNotification().getObject(),getNoonModification().getObject());
 		return sfb;
 	}
 }
--- cens_web/src/main/java/com/aehtiopicus/cens/configuration/TestScheduler.java
+++ cens_web/src/main/java/com/aehtiopicus/cens/configuration/TestScheduler.java
@@ -4,12 +4,27 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import com.aehtiopicus.cens.domain.entities.MiembroCens;
+import com.aehtiopicus.cens.domain.entities.RestRequest;
+import com.aehtiopicus.cens.service.cens.MiembroCensService;
+import com.aehtiopicus.cens.util.Utils;
+
 public class TestScheduler extends QuartzJobBean {
 
 	@Override
 	protected void executeInternal(JobExecutionContext context)
 			throws JobExecutionException {
-		System.out.print("vaca");
+		
+			System.out.println(context.getTrigger().getDescription());
+			RestRequest rq = new RestRequest();
+			rq.setPage(1);
+			rq.setRow(5000);
+			rq.setSord("asc");
+			for(MiembroCens mc: ((MiembroCensService)context.getJobDetail().getJobDataMap().get("miembroCensService")).listMiembrosCens(rq)){
+				System.out.println(Utils.getSon().toJson(mc));
+			}
+		
+	
 		
 	}
 
