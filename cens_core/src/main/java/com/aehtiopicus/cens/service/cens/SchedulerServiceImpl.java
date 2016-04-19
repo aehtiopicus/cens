125f346c63232bf60456e5b351857169bc678b10
aehtiopicus <aehtiopicus@gmail.com>
2016-04-18 23:55:14 -0300

    changes

--- cens_core/src/main/java/com/aehtiopicus/cens/service/cens/SchedulerServiceImpl.java
+++ cens_core/src/main/java/com/aehtiopicus/cens/service/cens/SchedulerServiceImpl.java
@@ -1,8 +1,26 @@
 package com.aehtiopicus.cens.service.cens;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SchedulerServiceImpl implements SchedulerService{
 
+	 
+	private SchedulerFactoryBean schedulerFactoryBean;
+
+	@Autowired
+	public void setSchedulerFactoryBean(SchedulerFactoryBean schedulerFactoryBean) {
+		this.schedulerFactoryBean = schedulerFactoryBean;
+		schedulerFactoryBean.getScheduler().get
+
+	    Scheduler scheduler = schedulerFactoryBean.getScheduler();
+	    TriggerKey triggerKey = new TriggerKey("timeSyncTrigger");
+	    CronTriggerImpl trigger = (CronTriggerImpl) scheduler.getTrigger(triggerKey);
+	    trigger.setCronExpression(newCronExpression );
+	    scheduler.rescheduleJob(triggerKey, trigger);
+	}
+	
+	
 }
